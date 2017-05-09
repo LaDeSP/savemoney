a0956e8917d9cd2c64e7f92320f44052b0146daa
jorge <jorge-uk1@hotmail.com>
2017-05-09 15:17:00 -0300

    teste do segundo dia para ligar o git hub

--- Savemoney/src/go.java
+++ Savemoney/src/go.java
@@ -1,6 +1,8 @@
 
 public class go {
-	public static void main(String[] args) {
-system.out.println("teste de envio ");
-	}
+	
+	public static void main(String[] args)	
+		{
+		system.out.println("teste de envio ");
+		}
 }
